74dfd479ae84d943ad7385c69357514ea701aed7
Matifq14 <matias15963@hotmail.com>
2025-04-12 21:42:52 -0300

    ejercicio 3 y 4 resuelto

---
+++ js/ejercicio3.js
@@ -0,0 +1,5 @@
+const pi = 3.14
+let radio = 7.5
+
+let area = pi * (radio ** 2)
+console.log(area)
---
+++ js/ejercicio4.js
@@ -0,0 +1,7 @@
+let lado = 4
+
+let area = 2 * lado
+let perimetro = lado + lado + lado + lado
+
+console.log(area)
+console.log(perimetro)
