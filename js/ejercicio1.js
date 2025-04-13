1e09a035dc89d971567e21194174e57891c85269
NadiaE-Fernandez <naniifer98@gmail.com>
2025-04-12 21:13:37 -0300

    Agregando carpeta js y ejercicio1

---
+++ js/ejercicio1.js
@@ -0,0 +1,3 @@
+let integrantes= "Yésica de los Angeles Arias,Juan Pablo Farfán,Nadia Evangelina Fernandez,Matias Fernando Quispe";	
+ 
+console.log("Somos " + integrantes + "  y estamos aprendiendo javascript.");
