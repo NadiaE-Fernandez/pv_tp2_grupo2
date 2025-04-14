c42e733b3847e82fb5ba9f76f2c644417ef7b671
Arias Yesica <jesicaarias271@gmail.com>
2025-04-13 21:31:59 -0300

    pto6 terminado

--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -1,2 +1,12 @@
 const edades = [21, 19, 34, 38, 27, 30, 25, 16];
 console.log("Edades: "+ edades);
+
+const calcularPromedio = (lista) => {
+    let suma = 0;
+    for (let i = 0; i < lista.length; i++) {
+        suma += lista[i];
+    }
+    return suma / lista.length;
+};
+
+console.log("El promedio de las edades es: " + calcularPromedio(edades));
